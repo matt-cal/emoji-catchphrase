4cfa495ea2cec6ed3a72de8025a23695bd98ad04
matthew-callister <matthew.callister@wpengine.com>
2023-08-03 09:49:57 -0500

    Refactor to represent messages with list elements

--- client/src/pages/TestPage.js
+++ client/src/pages/TestPage.js
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from "react";
 import "./TestPage.css";
 import { get, post } from "../utilities.js";
 import { socket } from "../client-socket.js";
+import EmojiPicker from 'emoji-picker-react';
+
 
 const TestPage = (props) => {
 	const [inputValue, setInput] = useState("");
-	const [messages, setMessages] = useState("");
+	const [messages, setMessages] = useState([]);
 	const [roomID, setRoom] = useState("");
 
-	function addMessage() {
-		setMessages(messages + inputValue + "\n\n");
-		socket.emit("new-message", inputValue, roomID)
+	function addMessage(e) {
+		e.preventDefault();
+		setMessages([...messages, inputValue]);
+		socket.emit("new-message", inputValue, roomID);
 		setInput("");
 	}
 
@@ -22,15 +25,31 @@ const TestPage = (props) => {
 	}
 
 	socket.on("new-message", (message) => {
-		setMessages(messages + message + "\n\n");
+		setMessages([...messages, message]);
 	});
+
+	function addEmoji(emoji, e) {
+		setMessages([...messages, emoji.emoji]);
+		socket.emit("new-message", emoji.emoji, roomID);
+	}
 
 	return (
 		<div className="test-page-container">
 			<h1>Test Page</h1>
-			<pre id="messages">{messages}</pre>
-			<input type="text" id="input" placeholder="Message" value={inputValue} onChange={(e) => setInput(e.target.value)}/>
-			<button id="submit" title="Send Message" onClick={addMessage}>Send Message</button>
+			{/* <pre className="messages">{messages}</pre> */}
+
+			<ul className="messages">
+				{messages.map(message => {
+					return <li>{message}</li>
+				})}
+			</ul>
+
+			{/* <EmojiPicker className="emoji-picker" onEmojiClick={(emoji, e) => addEmoji(emoji, e)} height={360} width="50%" /> */}
+
+			<form onSubmit={(e) => addMessage(e)}>
+				<input type="text" id="input" placeholder="Message" value={inputValue} onChange={(e) => setInput(e.target.value)}/>
+				<input type="submit" id="submit" title="Send Message"></input>
+			</form>
 
 			<input type="text" id="input" placeholder="Room ID" value={roomID} onChange={(e) => setRoom(e.target.value)}/>
 			<button id="submit" onClick={submitRoom}>Set Room</button>
